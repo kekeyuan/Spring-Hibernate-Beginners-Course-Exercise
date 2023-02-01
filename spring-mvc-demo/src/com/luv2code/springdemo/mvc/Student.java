a2e96bee646f1816474b3e0a0c562da0fbf6d5c6
kekeyuan <yubaoxiaolin@gmail.com>
2023-02-01 22:03:57 +0200

    spring form tags- form tags

---
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -0,0 +1,29 @@
+package com.luv2code.springdemo.mvc;
+
+
+public class Student {
+
+	private String firstName;
+	private String lastName;
+
+	public Student() {
+		
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstname(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastname(String lastName) {
+		this.lastName = lastName;
+	}
+
+}
