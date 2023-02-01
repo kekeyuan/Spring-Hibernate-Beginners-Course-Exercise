e69dd5caad3e236708af5dc9099fe98d044072a5
kekeyuan <yubaoxiaolin@gmail.com>
2023-02-01 22:44:55 +0200

    drop-down lists

--- spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
+++ spring-mvc-demo/WebContent/WEB-INF/view/student-confirmation.jsp
@@ -8,7 +8,9 @@
 <body>
 
 The student is confirmed:${student.firstName} ${student.lastName}
+<br><br>
 
+Country:${student.country}
 
 
 </body>
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Student.java
@@ -1,13 +1,25 @@
 package com.luv2code.springdemo.mvc;
 
+import java.util.LinkedHashMap;
 
 public class Student {
 
 	private String firstName;
 	private String lastName;
 
+	private String country;
+	
+	private LinkedHashMap<String,String> countryOptions;
+	
 	public Student() {
+		// populate country options: used ISO country code
+		countryOptions = new LinkedHashMap<>();
 		
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("US", "United States of America");		
 	}
 
 	public String getFirstName() {
@@ -26,4 +38,18 @@ public class Student {
 		this.lastName = lastName;
 	}
 
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+
+	
+	
 }
