2b3ac22cca63369a43295ae348f9bdbda9bfc74f
kekeyuan <yubaoxiaolin@gmail.com>
2023-02-03 21:14:32 +0200

    string input for integer fields

--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/Customer.java
@@ -10,19 +10,18 @@ public class Customer {
 
 	private String firstName;
 	
-	@NotNull
+	@NotNull(message="is required")
 	@Size(min=1,message="is required")
 	private String lastName;
 
-	
+	@NotNull(message="is required")
 	@Min(value=0,message="must be greater than or equal to zero")
 	@Max(value=10,message="must be less than or equal to zero")
-	private int freePasses;
+	private Integer freePasses;
 	
 	@Pattern(regexp="^[a-zA-Z0-9]{5}", message="only 5 chars/digits")
 	private String postalCode;
 	
-
 	public String getFirstName() {
 		return firstName;
 	}
@@ -39,11 +38,11 @@ public class Customer {
 		this.lastName = lastName;
 	}
 
-	public int getFreePasses() {
+	public Integer getFreePasses() {
 		return freePasses;
 	}
 
-	public void setFreePasses(int freePasses) {
+	public void setFreePasses(Integer freePasses) {
 		this.freePasses = freePasses;
 	}
 
--- spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
+++ spring-mvc-demo/src/com/luv2code/springdemo/mvc/CustomerController.java
@@ -38,10 +38,15 @@ public class CustomerController {
 	
 	@RequestMapping("/processForm")
 	public String processForm(
+			
 			@Valid @ModelAttribute("customer") Customer theCustomer,
 			BindingResult theBindingResult) {
 		
 		System.out.println("Last name: |" + theCustomer.getLastName() + "|");
+		
+		System.out.println("Binding result:" + theBindingResult);
+		
+		System.out.println("\n\n\n\n");
 		
 		if (theBindingResult.hasErrors()) {
 			return "customer-form";
